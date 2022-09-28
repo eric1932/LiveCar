35aac4ebe798d9e17cae3452827519ed9f7c6e74
Eric Liu <eric1932@users.noreply.github.com>
2022-09-28 01:17:01 -0500

    [fix] wheel rotation (both direction); car lights

--- Assets/External Assets/Unlock_Assets/Shared/Scripts/CarComponents.cs
+++ Assets/External Assets/Unlock_Assets/Shared/Scripts/CarComponents.cs
@@ -6,30 +6,31 @@ using UnityEngine.UI;
 
 
 
-public class CarComponents : MonoBehaviour {
+public class CarComponents : MonoBehaviour
+{
 
 	public bool blink;
 	[Header("Lights")]
 	public bool frontLightsOn;
 	public bool brakeEffectsOn;
 	[Space(5)]
-	public GameObject 			brakeEffects;
-	public GameObject 			frontLightEffects;
-	public GameObject 			reverseEffect;
+	public GameObject brakeEffects;
+	public GameObject frontLightEffects;
+	public GameObject reverseEffect;
 	[Header("Needles")]
-	public Transform			SpeedNeedle;
-	public Vector2				SpeedNeedleRotateRange	= Vector3.zero; 
-	private Vector3 			SpeedEulers				=   Vector3.zero;
-	public Transform			RpmNeedle;
-	public Vector2				RpmNeedleRotateRange  	= Vector3.zero; 
-	private Vector3 			RpmdEulers				=   Vector3.zero;
-	public float				_NeedleSmoothing 		= 1.0f;
-	public Transform 			steeringWheel;
+	public Transform SpeedNeedle;
+	public Vector2 SpeedNeedleRotateRange = Vector3.zero;
+	private Vector3 SpeedEulers = Vector3.zero;
+	public Transform RpmNeedle;
+	public Vector2 RpmNeedleRotateRange = Vector3.zero;
+	private Vector3 RpmdEulers = Vector3.zero;
+	public float _NeedleSmoothing = 1.0f;
+	public Transform steeringWheel;
 
-	private float 				rotateNeedles			= 0.0f;
+	private float rotateNeedles = 0f;
 	[Header("Wheels")]
-	public Transform 			wheel_FR;
-	public Transform 			wheel_FL;
+	public Transform wheel_FR;
+	public Transform wheel_FL;
 	[Header("Panel Texts")]
 	public Text txtSpeed, txtRPM;
 	public Slider sliderRPM;
@@ -37,89 +38,102 @@ public class CarComponents : MonoBehaviour {
 	private IEnumerator coroutine;
 
 
-	void Start () {
-		blink 			= true;
-		frontLightsOn 	= true;
-		brakeEffectsOn 	= true;
+	void Start()
+	{
+		blink = true;
+		frontLightsOn = true;
+		brakeEffectsOn = true;
 
 		if (SpeedNeedle) SpeedEulers = SpeedNeedle.localEulerAngles;
 		if (RpmNeedle) RpmdEulers = RpmNeedle.localEulerAngles;
 
-		coroutine = WaitLights(2.0f);
+		coroutine = WaitLights(2.5f);
 		StartCoroutine(coroutine);
 
-	
-	}
-	
 
-	void Update () {
-		if (blink) {
-			TurnOnFrontLights ();
-			TurnOnBackLights ();
+	}
+
+
+	void Update()
+	{
+		if (blink)
+		{
+			TurnOnFrontLights();
+			TurnOnBackLights();
 		}
 
-		if (SpeedNeedle) {
+		if (SpeedNeedle)
+		{
 
-				Vector3 temp = new Vector3 (SpeedEulers.x, SpeedEulers.y, Mathf.Lerp (SpeedNeedleRotateRange.x, SpeedNeedleRotateRange.y, (rotateNeedles)));
-				SpeedNeedle.localEulerAngles = Vector3.Lerp (SpeedNeedle.localEulerAngles, temp, Time.deltaTime * _NeedleSmoothing);
+			Vector3 temp = new Vector3(SpeedEulers.x, SpeedEulers.y, Mathf.Lerp(SpeedNeedleRotateRange.x, SpeedNeedleRotateRange.y, (rotateNeedles)));
+			SpeedNeedle.localEulerAngles = Vector3.Lerp(SpeedNeedle.localEulerAngles, temp, Time.deltaTime * _NeedleSmoothing);
 
 		}
 
 		if (RpmNeedle)
 		{
-			Vector3 temp = new Vector3( RpmdEulers.x,RpmdEulers.y,Mathf.Lerp( RpmNeedleRotateRange.x, RpmNeedleRotateRange.y,	(rotateNeedles)));
-			RpmNeedle.localEulerAngles = Vector3.Lerp( RpmNeedle.localEulerAngles, temp, Time.deltaTime * _NeedleSmoothing);
+			Vector3 temp = new Vector3(RpmdEulers.x, RpmdEulers.y, Mathf.Lerp(RpmNeedleRotateRange.x, RpmNeedleRotateRange.y, (rotateNeedles)));
+			RpmNeedle.localEulerAngles = Vector3.Lerp(RpmNeedle.localEulerAngles, temp, Time.deltaTime * _NeedleSmoothing);
 		}
 
-		if (steeringWheel != null) {
+		if (steeringWheel != null)
+		{
 			Vector3 eulers = steeringWheel.localRotation.eulerAngles;
 			Vector3 wheelsEulers = wheel_FL.localRotation.eulerAngles;
-			eulers.z = rotateNeedles * 15.0f;
-			wheelsEulers.y = -rotateNeedles * 15.0f;
+			eulers.z = (rotateNeedles - 1.25f) * 15.0f;
+			wheelsEulers.y = -(rotateNeedles - 1.25f) * 15.0f;
 
 
-			steeringWheel.localRotation = Quaternion.Slerp (steeringWheel.localRotation, Quaternion.Euler (eulers), Time.deltaTime * 2.5f);
+			steeringWheel.localRotation = Quaternion.Slerp(steeringWheel.localRotation, Quaternion.Euler(eulers), Time.deltaTime * 2.5f);
 
-			wheel_FL.localRotation = Quaternion.Slerp (wheel_FL.localRotation, Quaternion.Euler (wheelsEulers), Time.deltaTime * 2.5f);
-			wheel_FR.localRotation = Quaternion.Slerp (wheel_FR.localRotation, Quaternion.Euler (wheelsEulers), Time.deltaTime * 2.5f);
+			wheel_FL.localRotation = Quaternion.Slerp(wheel_FL.localRotation, Quaternion.Euler(wheelsEulers), Time.deltaTime * 2.5f);
+			wheel_FR.localRotation = Quaternion.Slerp(wheel_FR.localRotation, Quaternion.Euler(wheelsEulers), Time.deltaTime * 2.5f);
 
 
 
 		}
 
 		if (txtSpeed)
-			txtSpeed.text = ((int)(rotateNeedles * 100.0f)).ToString ();// + " km/h";
-		if (txtRPM) 
-		txtRPM.text = ((int)(rotateNeedles * 1000.0f)).ToString ();
+			txtSpeed.text = ((int)(rotateNeedles * 100.0f)).ToString();// + " km/h";
+		if (txtRPM)
+			txtRPM.text = ((int)(rotateNeedles * 1000.0f)).ToString();
 		if (sliderRPM)
 			sliderRPM.value = (rotateNeedles * 1000.0f);
-		
+
 	}
 
 	public void TurnOnFrontLights()
 	{
-		if (frontLightsOn) {
-			frontLightEffects.SetActive (true);
+		if (frontLightsOn)
+		{
+			frontLightEffects.SetActive(true);
 			rotateNeedles += Time.deltaTime;
-		} else {
-			frontLightEffects.SetActive (false);
+		}
+		else
+		{
+			frontLightEffects.SetActive(false);
 			rotateNeedles -= Time.deltaTime;
 		}
 	}
 
 	public void TurnOnBackLights()
 	{
-		if (brakeEffectsOn) {
-			brakeEffects.SetActive (true);
-		} else {
-			brakeEffects.SetActive (false);
+		if (brakeEffectsOn)
+		{
+			brakeEffects.SetActive(true);
+		}
+		else
+		{
+			brakeEffects.SetActive(false);
 		}
 	}
 
 
 
-	private IEnumerator WaitLights(float waitTime) {
-		while (true) {
+	private IEnumerator WaitLights(float waitTime)
+	{
+		while (true)
+		{
 			yield return new WaitForSeconds(waitTime);
 			frontLightsOn = !frontLightsOn;
 			brakeEffectsOn = !brakeEffectsOn;
